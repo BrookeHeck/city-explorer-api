e61facc7ca71f0df024742244fd12c37e040795b
Brooke Heck <b19heck@gmail.com>
2022-08-24 12:08:14 -0700

    weather data is pulled from weather api

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const allData = require('./weather.json');
+const axios = require('axios').default;
 
 
 // Use
 const app = express();
 const PORT = process.env.PORT || 3002;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 app.use(cors());
 
 // Routes
@@ -20,12 +22,20 @@ app.get('/', (request, response) => {
 });
 
 app.get('/weather', (request, response, next) => {
-  try {
-    let forecastObjArr = (getForecast(request.query.city));
-    response.send(forecastObjArr);
-  } catch(e) {
-    next(e);
-  }
+    axios.get('https://api.weatherbit.io/v2.0/current', {
+      params: {
+        key: WEATHER_API_KEY,
+        lat: request.query.lat,
+        lon: request.query.lon
+      }
+    }).then(function (weatherData) {
+      let forecast = new Forecast(weatherData.data.data[0]);
+      response.send(forecast);
+    }).catch(function (error) {
+      next(error);
+    }).then(function () {
+      console.log('Data not found');
+    });
 });
 
 app.get('*', (request, response) => {
@@ -39,23 +49,11 @@ app.use((error, request, response, next) => {
 
 // Classes
 class Forecast {
-  constructor(description, date) {
-    this.description = description;
-    this.date = date;
+  constructor(weatherData) {
+    this.temp = weatherData.app_temp;
+    this.description = weatherData.weather.description;
+    this.date = weatherData.ob_time;
   }
-}
-
-let getForecast = (searchQuery) => {
-  let weatherObj = allData.find(obj => obj.city_name === searchQuery);
-  let objData = weatherObj.data;
-  let forecastArr = objData.reduce((accumulator, current) => {
-    let description = `Low of ${current.app_min_temp}, high of ${current.app_min_temp} with ${current.weather.description.toLowerCase()}`;
-    let date = current.valid_date;
-    let forecast = new Forecast(description, date);
-    accumulator.push(forecast);
-    return accumulator;
-  }, []);
-  return forecastArr;
 }
 
 
